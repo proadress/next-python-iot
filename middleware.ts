a55fc1aac5bcf198c92dbe37e74c14ee2590e73b
cloud <yc359032@gmail.com>
2024-02-10 17:11:47 +0800

    0

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,24 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getToken } from './app/lib/cookie';
 
-export default async function middleware(request: Request) {
+export default async function middleware(request: NextRequest) {
   const token = getToken();
+  console.log(process.env.VERCEL_URL);
+  return NextResponse.json(process.env.VERCEL_URL);
 
-  if (token) {
-    const res = await fetch(`${process.env.VERCEL_URL}/api/auth/user`, {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
-      }
-    })
-    if (!res.ok) {
-      return NextResponse.rewrite(new URL('/login', request.url));
-    }
-  } else {
-    return NextResponse.rewrite(new URL('/login', request.url));
-  }
+  // if (token) {
+  //   const res = await fetch(`${process.env.VERCEL_URL}/api/auth/user`, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": "Bearer " + token
+  //     }
+  //   })
+  //   if (!res.ok) {
+  //     return NextResponse.rewrite(new URL('/login', request.url));
+  //   }
+  // } else {
+  //   return NextResponse.rewrite(new URL('/login', request.url));
+  // }
 }
 //protected
 export const config = {
